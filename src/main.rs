7f1f94dc1bf980e3cbe3ef29912cb2ffd7009a18
sch <sch@anadeainc.com>
2025-01-02 00:26:37 +0200

    Base content read.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+use reqwest;
+use std::fs;
+use std::io::Read;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    let data = fs::read_to_string("input/input-1.txt").unwrap();
+
+    println!("Data: {}", data);
 }
