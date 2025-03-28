943082c386643c848bf80949dc7982b6737fb9ec
sch <sch@anadeainc.com>
2025-03-28 09:40:02 +0200

    add command arguments

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,10 @@ use std::fs;
 pub mod events;
 
 pub fn read_input(year: &i32, day: &i32) -> String {
-    fs::read_to_string(format!("inputs/year_{}/day_{:02}.txt", year, day))
+    fs::read_to_string(get_input_path(year, day))
         .expect("Failed to read input file")
+}
+
+pub fn get_input_path(year: &i32, day: &i32) -> String {
+    format!("inputs/year_{}/day_{:02}.txt", year, day)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
-use advent_of_code::events::year_2015::day_01::solve;
-use advent_of_code::read_input;
+use std::collections::HashMap;
+use advent_of_code::{events, read_input};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Event year to run
+    #[arg(short, long)]
+    year: Option<i32>,
+
+    /// Day of the event to run
+    #[arg(short, long)]
+    day: Option<i32>,
+}
 
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
+    println!("year: {:?}, day: {:?}", args.year, args.day);
+
     let input = read_input(&2015, &1);
-    println!("{:?}", solve(&input));
+    println!("{:?}", events::year_2015::day_01::solve(&input));
 
     // let input_line = String::new();
     // let bomb_dir = input_line.trim().to_string();
