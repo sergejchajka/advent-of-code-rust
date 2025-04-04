d04eaa6ba2f43504efc68f87db57487f23b91a29
sch <sch@anadeainc.com>
2025-04-04 22:05:47 +0300

    small rename

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ pub fn read_input(year: &i32, day: &i32) -> String {
 }
 
 pub fn get_input_path(year: &i32, day: &i32) -> String {
-    format!("events_data/year_{}/day_{:02}/input.txt", year, day)
+    format!("data/year_{}/day_{:02}/input.txt", year, day)
 }
 
 #[cfg(test)]
@@ -19,7 +19,7 @@ mod tests {
 
     #[test]
     fn test_get_input_path() {
-        assert_eq!("events_data/year_2015/day_14/input.txt", get_input_path(&2015, &14));
-        assert_eq!("events_data/year_2000/day_01/input.txt", get_input_path(&2000, &1));
+        assert_eq!("data/year_2015/day_14/input.txt", get_input_path(&2015, &14));
+        assert_eq!("data/year_2000/day_01/input.txt", get_input_path(&2000, &1));
     }
 }
